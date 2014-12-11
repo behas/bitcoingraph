7e971df9d8e6565d7ffa7ab9b72b0d6615a7e551
Bernhard Haslhofer <bernhard.haslhofer@ait.ac.at>
2014-12-11 12:35:39 +0100

    finished blockchain tests

--- tests/test_blockchain.py
+++ tests/test_blockchain.py
@@ -4,10 +4,39 @@ from tests.rpc_mock import BitcoinProxyMock
 
 from bitcoingraph.blockchain import BlockChain
 
-BH1 = "000000000003ba27aa200b1cecaad478d2b00432346c3f1f3986da1afd33e506"
-BH1_HEIGHT = 100000
+BH1 = "000000000002d01c1fccc21636b607dfd930d31d01c3a62104612a1719011250"
+BH1_HEIGHT = 99999
+BH2 = "000000000003ba27aa200b1cecaad478d2b00432346c3f1f3986da1afd33e506"
+BH2_HEIGHT = 100000
+BH3 = "00000000000080b66c911bd5ba14a74260057311eaeb1982802f7010f1a9f090"
+BH3_HEIGHT = 100001
 
 TX1 = "8c14f0db3df150123e6f3dbbf30f8b955a8249b62ac1d1ff16284aefa3d06d87"
+
+
+class TestBlock(unittest.TestCase):
+
+    def setUp(self):
+        self.bitcoin_proxy = BitcoinProxyMock()
+        self.blockchain = BlockChain(self.bitcoin_proxy)
+
+    def test_height(self):
+        block = self.blockchain.get_block_by_hash(BH1)
+        self.assertEqual(block.height, BH1_HEIGHT)
+
+    def test_hash(self):
+        block = self.blockchain.get_block_by_hash(BH1)
+        self.assertEqual(block.hash, BH1)
+
+    def test_nextblockhash(self):
+        block = self.blockchain.get_block_by_hash(BH1)
+        self.assertTrue(block.hasnextblock)
+        block = self.blockchain.get_block_by_hash(BH3)
+        self.assertFalse(block.hasnextblock)
+
+    def test_nextblock(self):
+        block = self.blockchain.get_block_by_hash(BH1)
+        self.assertEqual(block.nextblock.height, BH2_HEIGHT)
 
 
 class TestBlockchain(unittest.TestCase):
