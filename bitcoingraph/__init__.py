0762929047076c4db978fa8bf3aba9c98eeb2b83
Bernhard Haslhofer <bernhard.haslhofer@ait.ac.at>
2015-01-13 10:25:15 +0100

    added exception handling

--- bitcoingraph/__init__.py
+++ bitcoingraph/__init__.py
@@ -4,6 +4,8 @@ __version__ = '0.1dev'
 
 import csv
 
+from bitcoingraph.blockchain import BlockchainException
+
 
 def generate_tx_graph(blockchain, start_block, end_block, output_file):
     cvs_writer = csv.writer(output_file, delimiter=';',
@@ -12,14 +14,19 @@ def generate_tx_graph(blockchain, start_block, end_block, output_file):
                          'timestamp', 'block_height'])
     for block in blockchain.get_blocks_in_range(start_block, end_block):
         for tx in block.transactions:
-            txid = tx.id
-            timestamp = tx.time
-            for bc_flow in tx.bc_flows:
-                src_addr = bc_flow['src']
-                if src_addr is None:
-                    src_addr = "N/A"
-                tgt_addr = bc_flow['tgt']
-                value = bc_flow['value']
-                block_height = block.height
-                cvs_writer.writerow([txid, src_addr, tgt_addr,
-                                     value, timestamp, block_height])
+            try:
+                txid = tx.id
+                timestamp = tx.time
+                for bc_flow in tx.bc_flows:
+                    src_addr = bc_flow['src']
+                    if src_addr is None:
+                        src_addr = "NULL"
+                    tgt_addr = bc_flow['tgt']
+                    if tgt_addr is None:
+                        tgt_addr = "NULL"
+                    value = bc_flow['value']
+                    block_height = block.height
+                    cvs_writer.writerow([txid, src_addr, tgt_addr,
+                                         value, timestamp, block_height])
+            except BlockchainException as exc:
+                print(exc)
