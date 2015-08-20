7d79e5dd55133d226f13a28699d5f9a5340e370b
Roman Karl <roman.karl@ait.ac.at>
2015-08-20 16:30:14 +0200

    add path query

--- bitcoingraph/graphdb.py
+++ bitcoingraph/graphdb.py
@@ -1,6 +1,5 @@
 
 import requests
-from bitcoingraph.blockchain import Address
 
 
 class GraphDB:
@@ -13,12 +12,19 @@ class GraphDB:
         self.url = 'http://{}:{}/db/data/transaction/commit'.format(host, port)
 
     def get_address(self, address):
-        return Address(self.address_query(address))
+        statement = 'MATCH (a:Address {address: {address}})-[r]-t RETURN a.address, t.txid, type(r), r.value'
+        return Address(self.query(statement, {'address': address}))
 
-    def address_query(self, address):
+    def get_path(self, address1, address2):
+        statement = '''MATCH p = shortestpath (
+                (start:Address {address: {address1}})-[*]->(end:Address {address: {address2}})
+            ) RETURN p'''
+        return Path(self.query(statement, {'address1': address1, 'address2': address2}))
+
+    def query(self, statement, parameters):
         payload = {'statements': [{
-            'statement': 'MATCH (a:Address {address: {address}})-[r]-t RETURN a.address, t.txid, type(r), r.value',
-            'parameters': {'address': address}
+            'statement': statement,
+            'parameters': parameters
         }]}
         headers = {
             'Accept': 'application/json; charset=UTF-8',
@@ -26,3 +32,36 @@ class GraphDB:
         }
         r = requests.post(self.url, auth=(self.user, self.password), headers=headers, json=payload)
         return r.json()
+
+
+class Address:
+
+    def __init__(self, raw_data):
+        self._raw_data = raw_data
+
+    @property
+    def data(self):
+        return self._raw_data['results'][0]['data']
+
+    @property
+    def address(self):
+        return self.data[0]['row'][0]
+
+    @property
+    def bc_flows(self):
+        return map(self.convert_row, self.data)
+
+    @staticmethod
+    def convert_row(raw_data):
+        row = raw_data['row']
+        return {'txid': row[1], 'type': row[2], 'value': row[3]}
+
+
+class Path:
+
+    def __init__(self, raw_data):
+        self._raw_data = raw_data
+
+    @property
+    def path(self):
+        return self._raw_data['results'][0]['data'][0]['row'][0]
