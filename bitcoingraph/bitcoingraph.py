f37ea498f6c0dc5a5286cf67c4af91a451646074
Roman Karl <roman.karl@ait.ac.at>
2015-11-11 15:37:54 +0100

    add basic docstrings for facade methods

--- bitcoingraph/bitcoingraph.py
+++ bitcoingraph/bitcoingraph.py
@@ -20,7 +20,8 @@ logger = logging.getLogger('bitcoingraph')
 
 class BitcoingraphException(Exception):
     """
-    Top-level exception raised when interacting with bitcoingraph library.
+    Top-level exception raised when interacting with bitcoingraph
+    library.
     """
 
     def __init__(self, msg, inner_exc):
@@ -32,21 +33,20 @@ class BitcoingraphException(Exception):
 
 
 class BitcoinGraph:
+    """Facade which provides the main access to this package."""
 
     def __init__(self, **config):
-
+        """Create an instance based on the configuration."""
         self.blockchain = self.__get_blockchain(config['blockchain'])
         if 'neo4j' in config:
             nc = config['neo4j']
             self.graph_db = GraphController(nc['host'], nc['port'], nc['user'], nc['pass'])
 
-    # Blockchain interfaces
     @staticmethod
     def __get_blockchain(config):
+        """Connect to Bitcoin Core (via JSON-RPC) and return a
+        Blockchain object.
         """
-        Connects to Bitcoin Core (via JSON-RPC) and returns a Blockchain object.
-        """
-
         try:
             logger.debug("Connecting to Bitcoin Core at {}".format(config['host']))
             bc_proxy = BitcoinProxy(**config)
@@ -58,42 +58,59 @@ class BitcoinGraph:
             raise BitcoingraphException("Couldn't connect to {}.".format(config['host']), exc)
 
     def get_transaction(self, tx_id):
+        """Return a transaction."""
         return self.blockchain.get_transaction(tx_id)
 
     def get_block_by_height(self, height):
+        """Return the block for a given height."""
         return self.blockchain.get_block_by_height(height)
 
     def get_block_by_hash(self, hash):
+        """Return a block."""
         return self.blockchain.get_block_by_hash(hash)
 
     def search_address_by_identity_name(self, term):
+        """Return an address that has an associated identity with
+        the given name.
+        """
         return self.graph_db.search_address_by_identity_name(term)
 
     def get_address_info(self, address, date_from, date_to):
+        """Return basic address information for the given
+        time period.
+        """
         return self.graph_db.get_address_info(address, date_from, date_to)
 
     def get_address(self, address, current_page, date_from, date_to,
                     rows_per_page=GraphController.rows_per_page_default):
+        """Return an address with its transaction uses in a given
+        time period.
+        """
         return self.graph_db.get_address(address, current_page, date_from, date_to, rows_per_page)
 
     def get_identities(self, address):
+        """Return a list of identities."""
         return self.graph_db.get_identities(address)
 
     def add_identity(self, address, name, link, source):
-        return self.graph_db.add_identity(address, name, link, source)
+        """Add an identity to an address."""
+        self.graph_db.add_identity(address, name, link, source)
 
     def delete_identity(self, identity_id):
+        """Delete an identity."""
         return self.graph_db.delete_identity(identity_id)
 
     def get_entity(self, id):
+        """Return an entity."""
         return self.graph_db.get_entity(id)
 
     def get_path(self, start, end):
+        """Return a path between addresses."""
         return self.graph_db.get_path(start, end)
 
-    def export(
-            self, start, end, output_path=None, plain_header=False, separate_header=True,
-            progress=None, deduplicate_transactions=True):
+    def export(self, start, end, output_path=None, plain_header=False, separate_header=True,
+               progress=None, deduplicate_transactions=True):
+        """Export the blockchain into CSV files."""
         if output_path is None:
             output_path = 'blocks_{}_{}'.format(start, end)
 
@@ -115,6 +132,9 @@ class BitcoinGraph:
                     sort(output_path, base_name + '.csv', '-u')
 
     def synchronize(self, max_blocks=None):
+        """Synchronise the graph database with the blockchain
+        information from the bitcoin client.
+        """
         start = self.graph_db.get_max_block_height() + 1
         blockchain_end = self.blockchain.get_max_block_height() - 2
         if max_blocks is None:
@@ -127,6 +147,9 @@ class BitcoinGraph:
 
 
 def compute_entities(input_path, sort_input=False):
+    """Read exported CSV files containing blockchain information and
+    export entities into CSV files.
+    """
     if sort_input:
         sort(input_path, 'rel_output_address.csv')
     sort(input_path, 'rel_input.csv', '-k 2 -t ,')
